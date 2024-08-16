5944ec177abf4f7d9b69c774a2b1405711441339
Yofti Milkessa <yoftahemilkessa@gmail.com>
2024-08-16 03:00:40 -0400

    quick fixes

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import useFetchAuthorEvents, {
   UseAuthorEventsParams,
 } from "./useFetchAuthorEvents";
 import useFetchFeed, { UseFetchFeedParams } from "./useFetchFeed";
+import useFetchProfile, { UseFetchProfileParams } from "./useFetchProfile";
 
 export {
   useFetchAuthorEvents,
