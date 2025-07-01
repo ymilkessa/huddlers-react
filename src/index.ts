4fdb8db57c3f3e5357be3d46d1797f4258d33bd6
Yofti Milkessa <yoftahemilkessa@gmail.com>
2025-07-01 16:11:36 -0400

    exported useFetchEvent -- was not exported before

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,15 @@ import useFetchEventsByAuthor, {
 import useFetchFeed, { UseFetchFeedParams } from "./useFetchFeed";
 import useFetchProfile, { UseFetchProfileParams } from "./useFetchProfile";
 import useFetchThread, { UseFetchThreadParams } from "./useFetchThread";
+import useFetchEvent, { UseFetchEventParams } from "./useFetchEvent";
 import { Event } from "huddlers";
 
 export {
   Event,
   useFetchAuthorEvents,
   useFetchEventsByAuthor,
+  useFetchEvent,
+  UseFetchEventParams,
   UseAuthorEventsParams,
   useFetchFeed,
   UseFetchFeedParams,
